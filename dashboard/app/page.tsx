86cc61409533b82307b9dc93d13535c4e6fed501
zoriespaycom <ezawahakimi@gmail.com>
2026-06-22 14:15:32 +0700

    Create page.tsx

---
+++ dashboard/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Merchant Dashboard</h1>
+
+      <ul>
+        <li>Active Subscriptions</li>
+        <li>Payroll Schedules</li>
+        <li>Total Payments</li>
+      </ul>
+    </div>
+  );
+}
